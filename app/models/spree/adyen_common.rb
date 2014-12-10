28445d426d2e7193334a676ae9296d33969ffde0
Rodrigo Vitulli <rovitulli@gmail.com>
2014-12-10 17:56:16 -0200

    changing shopper reference to document number

--- app/models/spree/adyen_common.rb
+++ app/models/spree/adyen_common.rb
@@ -229,7 +229,7 @@ module Spree
           unless payment.source.gateway_customer_profile_id.present?
 
             shopper = { #:reference => (payment.order.user_id.present? ? payment.order.user_id : payment.order.email),
-                        :reference => payment.order.email,
+                        :reference => payment.order.user.document_number,
                         :email => payment.order.email,
                         :ip => payment.order.last_ip_address,
                         :statement => "Order # #{payment.order.number}" }
