616937c8d0e3a7f5d97a1f939918fcf77e799450
brunozrk <brunozrk@gmail.com>
2016-05-06 08:57:25 -0300

    run specs in ci

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -37,6 +37,27 @@ RSpec.configure do |config|
   config.filter_run_excluding :external => true
 
   config.include Spree::Adyen::TestHelper
+
+  config.before(:all) { silence_output }
+  config.after(:all) { enable_output }
+end
+
+# Redirects stderr and stdout to /dev/null.
+def silence_output
+  @orig_stderr = $stderr
+  @orig_stdout = $stdout
+
+  # redirect stderr and stdout to /dev/null
+  $stderr = File.new('/dev/null', 'w')
+  $stdout = File.new('/dev/null', 'w')
+end
+
+# Replace stdout and stderr so anything else is output correctly.
+def enable_output
+  $stderr = @orig_stderr
+  $stdout = @orig_stdout
+  @orig_stderr = nil
+  @orig_stdout = nil
 end
 
 VCR.configure do |c|
